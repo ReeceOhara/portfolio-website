b59e587b7d00de01e635422b4d670569b8856320
Reece <ralexo99@gmail.com>
2019-07-06 22:01:15 -0400

    Added custom header and browser tab logos

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
     $('.list-image').on('mouseenter', (event) => {
         $(event.currentTarget).css('opacity', '0.6');
-        $(event.currentTarget).find('.image-caption').fadeIn('slow');
+        $(event.currentTarget).find('.image-caption').fadeIn('fast');
     }).on('mouseleave', (event) => {
         $(event.currentTarget).css('opacity', '1');
         $(event.currentTarget).find('.image-caption').fadeOut('fast');
